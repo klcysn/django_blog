0dbe2284c74d76cd1f356107d8c1962a6071091d
klcysn <klcysn@users.noreply.github.com>
2020-12-25 10:05:04 +0100

    Bind images

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog_app.urls")),
+    path('about/', TemplateView.as_view(template_name = "about.html")),
 ]
